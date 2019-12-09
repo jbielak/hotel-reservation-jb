a77707ae730cade23d9e8896365e782001be809d
jbielak <justyna_bielak@outlook.com>
2019-12-09 17:32:36 +0100

    Initial commit

---
+++ src/main/java/com/grapeup/hotelreservation/HotelReservationJbApplication.java
@@ -0,0 +1,13 @@
+package com.grapeup.hotelreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationJbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelReservationJbApplication.class, args);
+    }
+
+}
