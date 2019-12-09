3fe3fc24276a971f594003b0f5c2d2536c026da5
jbielak <justyna_bielak@outlook.com>
2019-12-10 00:15:55 +0100

    [ReservationController] Add get reservation

--- src/main/java/com/grapeup/hotelreservation/controller/ReservationController.java
+++ src/main/java/com/grapeup/hotelreservation/controller/ReservationController.java
@@ -2,8 +2,13 @@ package com.grapeup.hotelreservation.controller;
 
 import com.grapeup.hotelreservation.model.Reservation;
 import com.grapeup.hotelreservation.service.ReservationService;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,4 +25,20 @@ public class ReservationController {
         return reservationService.findAll();
     }
 
+    @GetMapping("/reservation/{id}")
+    public ResponseEntity<?> getReservation(@PathVariable Long id) {
+
+        return reservationService.findById(id)
+                .map(reservation -> {
+                    try {
+                        return ResponseEntity
+                                .ok()
+                                .location(new URI("/reservation/" + reservation.getId()))
+                                .body(reservation);
+                    } catch (URISyntaxException e ) {
+                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                    }
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
