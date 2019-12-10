ac95f19aed9ccdeafe72d6c547aca4cb72d275be
jbielak <jubi@grapeup.com>
2019-12-10 14:25:02 +0100

    [ReservationController] Add create reservation

--- src/main/java/com/grapeup/hotelreservation/controller/ReservationController.java
+++ src/main/java/com/grapeup/hotelreservation/controller/ReservationController.java
@@ -8,8 +8,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reservations")
 public class ReservationController {
 
+    private static final String RESERVATIONS_MAPPING = "/reservations/";
     private ReservationService reservationService;
 
     public ReservationController(ReservationService reservationService) {
@@ -36,12 +38,24 @@ public class ReservationController {
                     try {
                         return ResponseEntity
                                 .ok()
-                                .location(new URI("/reservations/" + reservation.getId()))
+                                .location(new URI(RESERVATIONS_MAPPING + reservation.getId()))
                                 .body(reservation);
                     } catch (URISyntaxException e ) {
                         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
                     }
                 })
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
+        Reservation newReservation = reservationService.save(reservation);
+        try {
+            return ResponseEntity
+                    .created(new URI(RESERVATIONS_MAPPING + newReservation.getId()))
+                    .body(newReservation);
+        } catch (URISyntaxException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
