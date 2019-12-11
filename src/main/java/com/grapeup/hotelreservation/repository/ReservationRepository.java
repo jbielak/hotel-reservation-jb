eddaedbaa3f4b998fd7671c0fb9899bb1a3482a3
jbielak <jubi@grapeup.com>
2019-12-11 15:30:26 +0100

    [ReservationService] Add findById

--- src/main/java/com/grapeup/hotelreservation/repository/ReservationRepository.java
+++ src/main/java/com/grapeup/hotelreservation/repository/ReservationRepository.java
@@ -3,8 +3,11 @@ package com.grapeup.hotelreservation.repository;
 import com.grapeup.hotelreservation.model.Reservation;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRepository {
 
     List<Reservation> findAll();
+    Optional<Reservation> findById(Long id);
+
 }
