da919f14c587ec5e3f861fb762aa991eb9791023
jbielak <jubi@grapeup.com>
2019-12-11 15:30:26 +0100

    [ReservationService] Add findById

--- src/test/java/com/grapeup/hotelreservation/service/ReservationServiceTest.java
+++ src/test/java/com/grapeup/hotelreservation/service/ReservationServiceTest.java
@@ -14,11 +14,13 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -70,4 +72,29 @@ public class ReservationServiceTest {
         assertThat(reservations.get(0).getEndDate(), is(mockReservation.getEndDate()));
         assertThat(reservations.get(0).getRoomId(), is(mockReservation.getRoomId()));
     }
+
+    @Test
+    public void shouldReturnReservationForId() {
+        when(reservationRepository.findById(anyLong())).thenReturn(Optional.of(mockReservation));
+
+        Optional<Reservation> reservation = reservationService.findById(1L);
+
+        assertThat(reservation.isPresent(), is(true));
+        assertThat(reservation.get().getId(), is(mockReservation.getId()));
+        assertThat(reservation.get().getUsername(), is(mockReservation.getUsername()));
+        assertThat(reservation.get().getNumberOfPeople(), is(mockReservation.getNumberOfPeople()));
+        assertThat(reservation.get().getStartDate(), is(mockReservation.getStartDate()));
+        assertThat(reservation.get().getEndDate(), is(mockReservation.getEndDate()));
+        assertThat(reservation.get().getRoomId(), is(mockReservation.getRoomId()));
+    }
+
+    @Test
+    public void shouldReturnNoReservationForUnknownId() {
+        when(reservationRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        Optional<Reservation> reservation = reservationService.findById(20L);
+
+        assertThat(reservation.isEmpty(), is(true));
+    }
+
 }
