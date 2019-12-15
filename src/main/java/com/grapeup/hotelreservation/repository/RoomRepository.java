d5e531157b596cb59a48e783e3c3fbb994f6f89e
jbielak <jubi@grapeup.com>
2019-12-15 11:04:58 +0100

    [RoomRepository] Add find by Id and by room type

--- src/main/java/com/grapeup/hotelreservation/repository/RoomRepository.java
+++ src/main/java/com/grapeup/hotelreservation/repository/RoomRepository.java
@@ -3,8 +3,14 @@ package com.grapeup.hotelreservation.repository;
 import com.grapeup.hotelreservation.model.Room;
 import com.grapeup.hotelreservation.model.RoomType;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface RoomRepository {
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
 
-    List<Room> findRoomsWithCapacity(RoomType roomType);
+    @Query("SELECT r FROM Room r WHERE r.roomType = :roomType")
+    List<Room> findRoomsWithCapacity(@Param("roomType") RoomType roomType);
 }
