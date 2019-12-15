7650f7d59877ddf0d89b3364b47a96571530aabf
jbielak <jubi@grapeup.com>
2019-12-15 11:04:58 +0100

    [RoomRepository] Add find by Id and by room type

---
+++ src/test/java/com/grapeup/hotelreservation/repository/RoomRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.grapeup.hotelreservation.repository;
+
+import com.grapeup.hotelreservation.model.Room;
+import com.grapeup.hotelreservation.model.RoomType;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@DataJpaTest
+public class RoomRepositoryTest {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Test
+    public void shouldFindAllBasicTypeRooms() {
+        List<Room> rooms = roomRepository.findRoomsWithCapacity(RoomType.BASIC);
+        assertThat(rooms, hasSize(2));
+    }
+
+    @Test
+    public void shouldNotFindSuiteRooms() {
+        List<Room> rooms = roomRepository.findRoomsWithCapacity(RoomType.SUITE);
+        assertThat(rooms, hasSize(0));
+    }
+
+    @Test
+    public void shouldFindRoomById() {
+        Optional<Room> room = roomRepository.findById(1L);
+
+        assertThat(room.isPresent(), is(true));
+        assertThat(room.get().getId(), is(1L));
+        assertThat(room.get().getRoomType(), is(RoomType.PENTHOUSE));
+        assertThat(room.get().getReservations(), is(hasSize(2)));
+    }
+
+    @Test
+    public void shouldNotFindRoomById() {
+        Optional<Room> room = roomRepository.findById(99L);
+
+        assertThat(room.isEmpty(), is(true));
+    }
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS room (
+  id          INTEGER PRIMARY KEY auto_increment,
+  room_type    VARCHAR NOT NULL,
+  PRIMARY KEY (id));
+
+INSERT INTO room VALUES(1, 'PENTHOUSE');
+INSERT INTO room VALUES(2, 'BASIC');
+INSERT INTO room VALUES(3, 'BASIC');
+
+DROP TABLE reservation;
+
+CREATE TABLE IF NOT EXISTS reservation (
+  id          INTEGER PRIMARY KEY auto_increment,
+  username    VARCHAR(64) NOT NULL,
+  number_of_people INTEGER NOT NULL,
+  start_date  DATE NOT NULL,
+  end_date    DATE NOT NULL,
+  room_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (room_id) REFERENCES room (id));
+
+INSERT INTO reservation VALUES(1, 'test_user', 7, '2020-12-17', '2020-12-20', 1);
+INSERT INTO reservation VALUES(2, 'test_user', 8, '2021-12-22', '2020-12-28', 1);
