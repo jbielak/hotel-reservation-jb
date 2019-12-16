c91362b43587e86e17d38ac54d43c64d316d1009
jbielak <jubi@grapeup.com>
2019-12-16 16:00:14 +0100

    Setup Eureka client

--- src/main/java/com/grapeup/hotelreservation/HotelReservationApplication.java
+++ src/main/java/com/grapeup/hotelreservation/HotelReservationApplication.java
@@ -2,8 +2,10 @@ package com.grapeup.hotelreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HotelReservationApplication {
 
     public static void main(String[] args) {
